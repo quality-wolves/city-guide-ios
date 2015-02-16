a1617962e0eaf4f06a9216a754e15a4a87f66e17
exeshneg <exeshneg@localhost>
2015-02-17 01:17:35 +0600

    added map view controller

---
+++ CityGuide/CityGuide/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  CityGuide
+//
+//  Created by Vladislav Zozulyak on 17.02.15.
+//  Copyright (c) 2015 The Empire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
