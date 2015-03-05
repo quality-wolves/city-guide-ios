9c46c2e4f7febf685de756efbc6fb66c1070790a
exeshneg <exeshneg@localhost>
2015-03-06 01:32:22 +0600

    fav icon

---
+++ CityGuide/CityGuide/FavouritesManager.h
@@ -0,0 +1,21 @@
+//
+//  FavouritesManager.h
+//  CityGuide
+//
+//  Created by Vladislav Zozulyak on 05.03.15.
+//  Copyright (c) 2015 The Empire. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "CityGuide-Swift.h"
+
+@interface FavouritesManager : NSObject
+
+- (void) addFavouriteHotspot: (Hotspot *) hotspot;
+- (void) removeHotspotFromFavourites: (Hotspot *) hotspot;
+- (BOOL) isFavourite: (Hotspot *) hotspot;
+- (NSArray *) allFavourites;
+
++ (FavouritesManager *) sharedManager;
+
+@end
