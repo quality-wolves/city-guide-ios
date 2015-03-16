3b13351f7687c0d46bc0e8b798d63f9a9dfcb496
Vladislav Zozulyak <vlad@arello-mobile.com>
2015-03-16 21:01:05 +0600

    hotspot details screen

--- CityGuide/CityGuide/HotspotDetailsCell.m
+++ CityGuide/CityGuide/HotspotDetailsCell.m
@@ -21,17 +21,50 @@
 @property (nonatomic, weak) IBOutlet UILabel *descriptionLabel;
 @property (nonatomic, weak) IBOutlet UIScrollView *scrollView;
 @property (weak, nonatomic) IBOutlet UIButton *favouriteButton;
+@property (weak, nonatomic) IBOutlet UIView *bottomContainer;
+@property (weak, nonatomic) IBOutlet UILabel *phoneLabel;
+@property (weak, nonatomic) IBOutlet UILabel *addressLabel;
+@property (weak, nonatomic) IBOutlet UILabel *webLabel;
+@property (weak, nonatomic) IBOutlet UILabel *addedToFavouritesLabel;
+
+
 @end
 
 @implementation HotspotDetailsCell
 
 - (IBAction)favouriteAction:(id)sender {
+    __weak HotspotDetailsCell *wself = self;
     _favouriteButton.selected = !_favouriteButton.selected;
+    
     if (_favouriteButton.selected) {
         [[FavouritesManager sharedManager] addFavouriteHotspot:_hotspot];
+        [UIView animateWithDuration:0.3 animations:^() {
+            wself.addedToFavouritesLabel.alpha = 1;
+        } completion: ^(BOOL finished) {
+            if (finished) {
+                [UIView animateKeyframesWithDuration:0.3f delay:1 options:0 animations:^() {
+                    wself.addedToFavouritesLabel.alpha = 0;
+                } completion:^(BOOL finished) {
+                    
+                }];
+            }
+        }];
     } else {
         [[FavouritesManager sharedManager] removeHotspotFromFavourites:_hotspot];
+        [wself.addedToFavouritesLabel.layer removeAllAnimations];
+        [UIView animateKeyframesWithDuration:0.3f delay:0 options:0 animations:^() {
+            wself.addedToFavouritesLabel.alpha = 0;
+        } completion:^(BOOL finished) {
+            
+        }];
     }
+}
+
+- (void) awakeFromNib {
+    self.bottomContainer.layer.borderColor = [[[UIColor lightGrayColor] colorWithAlphaComponent:0.6] CGColor];
+    self.bottomContainer.layer.borderWidth = 0.5;
+    self.bottomContainer.backgroundColor = [UIColor clearColor];
+    self.addedToFavouritesLabel.alpha = 0;
 }
 
 - (void)setHotspot:(Hotspot *)hotspot {
@@ -40,6 +73,9 @@
     self.titleLabel.text = hotspot.name;
     self.descriptionLabel.text = hotspot.desc;
     _favouriteButton.selected = [[FavouritesManager sharedManager] isFavourite:_hotspot];
+    self.addressLabel.text = @"Carrer d'Elisabets, 11, 08001, Barcelona";
+    self.phoneLabel.text = @"+34 932426280";
+    self.webLabel.text = @"Praktikhotels.com";
 
 }
 
