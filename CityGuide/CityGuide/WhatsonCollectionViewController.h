5930693f254ea40f0188a6f9e318ecf7e60c4d8d
polyakrecords <polyakrecords@localhost>
2015-04-23 03:11:49 +0600

    add whatson page

---
+++ CityGuide/CityGuide/WhatsonCollectionViewController.h
@@ -0,0 +1,14 @@
+//
+//  WhatsonCollectionViewController.h
+//  CityGuide
+//
+//  Created by Tikhon Polyakov on 23.04.15.
+//  Copyright (c) 2015 The Empire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "HotspotCollectionViewController.h"
+
+@interface WhatsonCollectionViewController : HotspotCollectionViewController
+
+@end
